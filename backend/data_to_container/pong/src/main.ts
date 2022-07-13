6d2536bfa4b27658f25df34a84a4f7d05eb8f662
csejault <clement.sejault@outlook.com>
2022-07-13 15:37:48 +0200

    feat:	start socket.io : create connection and socket.on for front emit

--- backend/data_to_container/pong/src/main.ts
+++ backend/data_to_container/pong/src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  //await app.listen(3000);
 }
 bootstrap();
+
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: ["http://pika.home:3000"],
+  },
+})
+
+io.on("connection", socket => {
+  console.log(socket.id)
+  socket.on('custom-event', (str) => {
+    console.log(" string received =", str)
+  })
+})
