68769a9e7f18ce0ea6921fd2a6b7f66a65ca03dc
Clement SEJAULT <csejault@e2r9p18.clusters.42paris.fr>
2022-08-03 13:52:47 +0200

    fix: import torm.forfeatur in test

--- backend/data_to_container/pong/src/test/test.module.ts
+++ backend/data_to_container/pong/src/test/test.module.ts
@@ -4,9 +4,12 @@ import { QuestionController } from './question.controller';
 import { TestService } from './test.service';
 import { QuestionService } from './question.service'
 import { BddModule } from 'src/bdd/bdd.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Quiz } from 'src/bdd/quiz.entity';
+import { Question } from 'src/bdd/question.entity';
 
 @Module({
-  imports: [BddModule],
+  imports: [BddModule, TypeOrmModule.forFeature([Quiz, Question])],
   controllers: [TestController, QuestionController],
   providers: [TestService, QuestionService]
 })
