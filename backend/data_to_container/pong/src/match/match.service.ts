34274a1b78ffae3ae88fbab0e17c06709c3f94d0
pika <pika@42paris.fr>
2022-08-17 18:12:41 +0200

    [BDD] match saveScore create get all matchuid works

--- backend/data_to_container/pong/src/match/match.service.ts
+++ backend/data_to_container/pong/src/match/match.service.ts
@@ -1,14 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { match } from 'src/bdd/matchs.entity';
 import { user } from 'src/bdd/users.entity';
 import { Repository } from 'typeorm';
 import { CreateMatchDto } from './dto/createMatch.dto';
 import { SaveScoreDto } from './dto/saveScore.dto';
-import { UserController } from 'src/user/user.controller';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class MatchService {
+	@Inject(UserService)
+	private readonly userService : UserService;
 	constructor(
 		@InjectRepository(match) private MatchRepository: Repository<match>,
 	) { }
@@ -18,8 +20,8 @@ export class MatchService {
 	}
 
 	async createMatch(matchToCreate: CreateMatchDto): Promise<match> {
-		var user1: user = await UserController.prototype.getUser(matchToCreate.user1uid);
-		var user2: user = await UserController.prototype.getUser(matchToCreate.user2uid);
+		var user1: user = await this.userService.getUser(matchToCreate.user1uid);
+		var user2: user = await this.userService.getUser(matchToCreate.user2uid);
 		return await this.MatchRepository.save({
 			user1: user1,
 			user2: user2,
