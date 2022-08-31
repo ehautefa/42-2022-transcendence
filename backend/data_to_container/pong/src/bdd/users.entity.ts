1305dbc72862bac4a2a479f10d47a259e5463c8d
pika <pika@42paris.fr>
2022-08-31 16:51:50 +0200

    [ADD] swagger

--- backend/data_to_container/pong/src/bdd/users.entity.ts
+++ backend/data_to_container/pong/src/bdd/users.entity.ts
@@ -1,35 +1,36 @@
 
 import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, OneToMany, JoinTable, ManyToMany } from 'typeorm'
+import { ApiProperty } from '@nestjs/swagger'
 
 @Entity('users')
 export class user extends BaseEntity {
 
+    @ApiProperty({ description: 'The uuid of the user'})
     @PrimaryGeneratedColumn("uuid")
     userUuid: string;
 
+    @ApiProperty({ example: 'ehautefa', description: 'The unique username (between 4 and 8 char)'})
     @Column('varchar')
     userName: string;
 
+    @ApiProperty({ description: 'The password of the user'})
     @Column('varchar')
     userPassword: string;
 
+    @ApiProperty({ example: 'false', description: 'The two factor authentication boolean'})
     @Column('boolean')
     twoFfactorAuth: boolean;
 
+    @ApiProperty({ example: '0', description: 'The number of win of the user'})
     @Column('smallint')
     wins: number;
 
+    @ApiProperty({ example: '0', description: 'The number of loose of the user'})
     @Column('smallint')
     losses: number;
 
+    @ApiProperty({ description: 'The list of user who are friend with the user'})
     @ManyToMany(() => user, usr => usr.userUuid)
     @JoinTable()
     friends: user[];
-
-
-    // @Column()
-
-    // match_history
-
-
 }
--- backend/data_to_container/pong/src/user/dto/endOfMatch.dto.ts
+++ backend/data_to_container/pong/src/user/dto/endOfMatch.dto.ts
@@ -1,10 +1,13 @@
 import { IsNotEmpty, Length } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class EndOfMatchDto {
 
+    @ApiProperty({ description: 'The winner uid' })
     @IsNotEmpty()
     winnerUuid: string;
 
+    @ApiProperty({ description: 'The loser uid' })
     @IsNotEmpty()
     loserUuid: string;
 }
