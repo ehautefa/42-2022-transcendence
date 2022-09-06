d1c3c0410bb1702610694ce9dee9c6d7fbc60fbe
Oscar LOZANO PEREZ <olozano-@e3r1p3.clusters.42paris.fr>
2022-09-06 11:31:20 +0200

    [ADD] enter key support

--- frontend/react/src/pages/chat/Chat.tsx
+++ frontend/react/src/pages/chat/Chat.tsx
@@ -1,10 +1,10 @@
 import NavBar from "../../components/NavBar/NavBar"
 import "./Chat.css"
 import { useState } from 'react'
-import {
-	CSSTransition,
-	TransitionGroup,
-  } from 'react-transition-group';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
+import {Route, NavLink, HashRouter} from 'react-router-dom'
+
+
 function Chat() {
 	var message: string;
 	const [messages, setMessages] = useState([""]);
@@ -14,16 +14,18 @@ function Chat() {
 	}
 
 	const sendMessage = (event:any) => {
+		event.preventDefault();
 		if (message) {
 			console.log("Message a envoyer : ", message);
 			// socket.emit("message", socket.id + ": " + message);
 			setMessages(prevValues => [...prevValues, message]);
 			message = "";
 		}
+		event.target.reset();
 	}
 
-
-	return (<div>
+	
+	return ( <div>
 		<NavBar />
 		<div className="mainComposant">
 			<div className="box">
@@ -46,7 +48,7 @@ function Chat() {
 						</CSSTransition>
 					))}
 				</TransitionGroup>
-				<form id="form" action="">
+				<form onSubmit={sendMessage}>
 					<input
 						id="input"
 						autoComplete="off"
@@ -54,13 +56,44 @@ function Chat() {
 						onChange={handleChange}
 						autoFocus
 					/>
-					<button type="reset" onClick={sendMessage}>
+					<button type="submit">
 						Send
 					</button>
 				</form>
 			</div>
 		</div>
+
 	</div>)
 }
 
 export default Chat
+
+
+{/*
+
+		<HashRouter>
+			
+
+
+		</HashRouter>
+
+
+
+
+<div>
+<h1>Simple SPA</h1>
+<ul className="header">
+  <li><NavLink to="/">Home</NavLink></li>
+  <li><NavLink to="/stuff">Stuff</NavLink></li>
+  <li><NavLink to="/contact">Contact</NavLink></li>
+</ul>
+<div className="content">
+  <Route path="/" component={Home}/>
+  <Route path="/stuff" component={Stuff}/>
+  <Route path="/contact" component={Contact}/>
+</div>
+</div>
+
+
+
+*/}
