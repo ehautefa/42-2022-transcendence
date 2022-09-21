9b277786bda5a23b899d45fcab3428ae08973031
MacBookPro de Pika <elisehautefaye@gmail.com>
2022-09-21 14:40:16 +0200

    [FIX] chatmodule and websocket

--- backend/data_to_container/pong/src/app.module.ts
+++ backend/data_to_container/pong/src/app.module.ts
@@ -10,12 +10,12 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MatchModule } from './match/match.module';
 import { PongModule } from './pong/pong.module';
-// import { PongGateway } from './pong/pong.gateway';
+import { ChatModule } from './chat/chat.module';
 import { StatusModule } from './status/status.module';
 import { StatusGateway } from './status/status.gateway';
 
 @Module({
-  imports: [TestModule, ApiModule, BddModule, UserModule, AuthModule, MatchModule, PongModule, StatusModule],
+  imports: [TestModule, ApiModule, ChatModule, BddModule, UserModule, AuthModule, MatchModule, PongModule, StatusModule],
   controllers: [AppController],
   providers: [AppService, StatusGateway],
 })
--- backend/data_to_container/pong/src/chat/chat.module.ts
+++ backend/data_to_container/pong/src/chat/chat.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BddModule } from 'src/bdd/bdd.module';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 
 @Module({
   providers: [ChatGateway, ChatService],
+  imports: [BddModule],
 })
 export class ChatModule {}
