c109705e5b2519b17341d195c63d6fd6528f20bd
Oscar Lozano <16002228+oscarlozano@users.noreply.github.com>
2022-10-08 18:01:28 +0200

    [TRY] creating rooms and filling them

--- frontend/react/src/pages/chat/Chat.tsx
+++ frontend/react/src/pages/chat/Chat.tsx
@@ -1,33 +1,54 @@
 import { useEffect, useState } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Popup from 'reactjs-popup';
-import { getSocketChat } from "../../App";
+import { User } from "../../type";
+import { getSocketChat, getSocketStatus } from "../../App";
 import NavBar from "../../components/NavBar/NavBar";
+import {getMyFriends, getMe} from "../myProfile/request"
 import "./Chat.css";
+
 //import {Route, NavLink, HashRouter} from 'react-router-dom'
 //import { User } from "../../type";
 
+const socketStatus = getSocketStatus();
+
+socketStatus.on('getUserUuid', () => {
+	socketStatus.emit('getUserUuid');
+})
+
 function Chat() {
     const socket = getSocketChat();
+	const emptyUser: User = { userUuid: "", userName: "" };
+	const [user, setUser] = useState(emptyUser);
+	const [friends, setFriends] = useState([]);
     const [open, setOpen] = useState(false);
     const [selectedRoom, setSelectedRoom] = useState("");
     const [messages, setMessages] = useState();
     const [channels, setChannels] = useState([]);
+	const [newMessage, setNewMessage] = useState("");
 
-    var userUuid = localStorage.getItem('uid');
-    if (userUuid === 'undefined' || userUuid === null)
-        userUuid = 'a433d950-3f11-11ed-b878-0242ac120002';
-    const [newChannel, setNewChannel] = useState("blaaa");
+    const [newChannel, setNewChannel] = useState("");
 
+	async function fetchUser() {
+		const user = await getMe();
+		const friends = await getMyFriends();
+		setFriends(friends);
+		socketStatus.emit('getFriendsStatus', friends, (data: any) => {
+			setFriends(data);
+		});
+		setUser(user);
+	}
 
-    useEffect(() => {
-        socket.emit('findAllPublicRooms', (rooms:any) => {setChannels(rooms)});
-        
+	useEffect(() => {
+		fetchUser();
+
+		socket.emit('findAllPublicRooms', (rooms:any) => {setChannels(rooms)});
         socket.on('room', (rooms:any) => {
             console.log('getting information');
             setChannels(rooms);
         });
-    });
+	}, []);
+
 
     useEffect(() => {
         socket.emit('getAllMessagesInRoom', selectedRoom, (msgs:any) => {
@@ -36,14 +57,20 @@ function Chat() {
 
     function makeRoom() {
         console.log('creating room ', newChannel);
-        socket.emit('createRoom', { name: newChannel, ownerId: userUuid, 
+        socket.emit('createRoom', { name: newChannel, ownerId: user.userUuid, 
             isProtected:false, password: "", type: 'public', 
-            userId:userUuid 
+            userId:user.userUuid 
         });
         console.log(channels);
+		setNewChannel("");
         setOpen(false);
     }
     
+	function sendMessage()
+	{
+		socket.emit('createMessage', {senderID: user.userUuid, message: newMessage, roomId: selectedRoom});
+		console.log('sending message: ', newMessage);
+	}
     
     return ( <div>
         <NavBar />
@@ -80,7 +107,7 @@ function Chat() {
             <div className="chat">
                 <TransitionGroup className="messages">
                     {messages ? (messages as any).map((message:any) => (
-                        message.sender.userUuid === userUuid ? 
+                        message.sender.userUuid === user.userUuid ? 
                         (<CSSTransition key={message} timeout={500} classNames="fade">
                         <div className="message_mine">me: {message}</div>
                         </CSSTransition>) : (<CSSTransition key={message} timeout={500} classNames="fade">
@@ -89,11 +116,11 @@ function Chat() {
                     ) : null
                     }
                 </TransitionGroup>
-                <form onSubmit={makeRoom}>
+                <form onSubmit={sendMessage}>
                     <input
                         autoComplete="off"
                         type="text"
-                        onChange={makeRoom}
+						onChange={(e) => setNewMessage(e.target.value)}
                         autoFocus
                     />
                     <button type="submit"> Send </button>
