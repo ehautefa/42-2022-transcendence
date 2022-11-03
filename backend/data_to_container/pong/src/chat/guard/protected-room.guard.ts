2ba326c611e309789366a8909eaf9a75da09b30b
Romain TROJANI-BIGOT <rotrojan@e1r3p1.clusters.42paris.fr>
2022-11-03 11:54:32 +0100

    Password are now working

--- backend/data_to_container/pong/src/chat/guard/protected-room.guard.ts
+++ backend/data_to_container/pong/src/chat/guard/protected-room.guard.ts
@@ -15,12 +15,13 @@ export class ProtectedRoomGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const dto: JoinRoomDto | ChangePasswordDto = context.switchToWs().getData();
     const room: Room = await this.roomRepository.findOneOrFail({
-      select: { hash: true },
+      select: { hash: true, type: true },
       where: { id: dto.roomId },
     });
     if (room.type !== RoomType.PROTECTED) return true;
     console.log('XXXXXXXXXXXXXXXXXXXXXXXXXX');
     if (await argon.verify(room.hash, dto.password)) return true;
+    console.log('invalid password');
     throw new WsException('Invalid password');
   }
 }
