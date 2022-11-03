dc4b18144444e123ec2ca5c32b9ec86d606b8af8
Clement SEJAULT <csejault@e2r3p11.clusters.42paris.fr>
2022-11-03 19:16:55 +0100

    up: merge all .env to feat subject.
    autogenerate ssl cert in the runtime for RP otherwise in build for
    back/front
    
    add samesite and httponly for cookies

--- backend/dev_entrypoint.sh
+++ backend/dev_entrypoint.sh
@@ -1,7 +1,3 @@
-apk add openssl
-if [ ! -f "/ect/ssl/pong/.pong.key" ]; then
-    openssl req -x509 -newkey rsa:4096 -keyout /etc/ssl/pong/.pong.key -out /etc/ssl/pong/pong.csr -days 365 -nodes -subj "/CN=$APP_HOST"
-fi
-
+#!/bin/sh
 npm ci
 npm run start:dev
