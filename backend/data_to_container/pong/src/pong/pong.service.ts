ba4452f4dd29cdd0146258edbfbd4a2c011337fa
Oscar LOZANO PEREZ <olozano-@e1r8p3.clusters.42paris.fr>
2022-11-07 10:30:48 +0100

    [FIX] Des p'tits numeros la

--- backend/data_to_container/pong/src/pong/pong.service.ts
+++ backend/data_to_container/pong/src/pong/pong.service.ts
@@ -128,19 +128,19 @@ export class PongService {
     }
 
     hitLeftPaddle(game: GameWindowState): boolean {
-        if (game.ballX < parseInt(process.env.PONG_PADDLE_LEFT_X)
-            && game.ballX > parseInt(process.env.PONG_PADDLE_LEFT_X) - 7
-            && game.ballY >= game.paddleLeftY  
-            && game.ballY + parseInt(process.env.PONG_BALL_DIAM) <= game.paddleLeftY + game.paddleSize)
+        if (game.ballX < parseInt(process.env.PONG_PADDLE_LEFT_X) + 5
+            && game.ballX > parseInt(process.env.PONG_PADDLE_LEFT_X) - 3
+            && game.ballY >= game.paddleLeftY - 4
+            && game.ballY + parseInt(process.env.PONG_BALL_DIAM) <= game.paddleLeftY + game.paddleSize + 4)
             return true;
         return false;
     }
 
     hitRightPaddle(game: GameWindowState): boolean {
         if (game.ballX + parseInt(process.env.PONG_BALL_DIAM) > parseInt(process.env.PONG_PADDLE_RIGHT_X)
-            && game.ballX < parseInt(process.env.PONG_PADDLE_RIGHT_X) + 7
-            && game.ballY >= game.paddleRightY 
-            && game.ballY + parseInt(process.env.PONG_BALL_DIAM) <= game.paddleRightY + game.paddleSize)
+            && game.ballX + parseInt(process.env.PONG_BALL_DIAM) < parseInt(process.env.PONG_PADDLE_RIGHT_X) + 3
+            && game.ballY >= game.paddleRightY - 4
+            && game.ballY + parseInt(process.env.PONG_BALL_DIAM) <= game.paddleRightY + game.paddleSize + 4)
             return true;
         return false;
     }
